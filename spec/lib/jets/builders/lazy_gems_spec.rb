225f846319ba631125fd18f5e9e9305c4d68ab4f
Tung Nguyen <tongueroo@gmail.com>
2018-12-11 04:02:22 +0000

    start spec

--- spec/lib/jets/builders/lazy_gems_spec.rb
+++ spec/lib/jets/builders/lazy_gems_spec.rb
@@ -6,14 +6,29 @@ describe Jets::Builders::LazyGems do
 
     context "within lambda 250MB total limit" do
       it "creates a single layer for gems" do
+        opt = File.exist?("#{stage_area}/opt")
+        gems = File.exist?("#{stage_area}/gems")
+        expect(opt).to be true
+        expect(gems).to be false
       end
     end
 
-    context "over lambda 250MB total limit" do
-      it "creates max size layer for gems" do
-      end
-
+    context "over lambda 250MB total limit and within limit after lazy loading" do
       it "creates gems.zip to be lazy loaded" do
+        opt = File.exist?("#{stage_area}/opt")
+        gems = File.exist?("#{stage_area}/gems")
+        expect(opt).to be true
+        expect(gems).to be true
+      end
+    end
+
+    context "over lambda 250MB total limit even after lazy loading" do
+      it "creates gems.zip to be lazy loaded" do
+        opt = File.exist?("#{stage_area}/opt")
+        gems = File.exist?("#{stage_area}/gems")
+        expect(opt).to be true
+        expect(gems).to be true
+        lazy_gems.halt # expect this to be called
       end
     end
   end
